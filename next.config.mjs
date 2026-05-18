fb276b622437bcf71821bae405bc2022bebfe1dd
narayangarodi108-gif <narayangarodi108@gmail.com>
2026-05-19 00:41:09 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  distDir: '.next',
+  trailingSlash: false,
+  output: 'standalone'
+};
 
 export default nextConfig;
-
